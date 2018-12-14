ab6310d2cd1ab33787289f5996a5b80eca9bcf5e
Derek Elliott <elliott_derek@bah.com>
2018-12-14 12:49:38 -0500

    Finish day6 parts one and two

--- day6.py
+++ day6.py
@@ -1,29 +1,73 @@
 class Field:
     def __init__(self, x_limit, y_limit):
-        self.field = [[{'location_id': '.', 'distance': -1} for i in range(x_limit + 1)] for j in range(y_limit + 1)]
+        self.field = [[{'location_id': '.', 'distances': [], 'total_distance': 0} for i in range(x_limit + 1)] for j in range(x_limit + 1)]
+        self.x_bounds = [0, x_limit]
+        self.y_bounds = [0, y_limit]
         self.locations = {}
         self.last_location = -1
 
+    def _distance(self, point_1, point_2):
+        return abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1])
+
+    def _fill_area(self, location, id):
+        for x, row in enumerate(self.field):
+            for y, item in enumerate(row):
+                dist = self._distance([x,y], location['center'])
+                if item['distances'] == [] or min(item['distances']) > dist:
+                    self.field[x][y]['location_id'] = str(id)
+                elif min(item['distances']) == dist:
+                    self.field[x][y]['location_id'] = '.'
+                self.field[x][y]['distances'].append(dist)
+
+    def _calculate_distance_sum(self):
+        for x, row in enumerate(self.field):
+            for y, item in enumerate(row):
+                self.field[x][y]['total_distance'] = sum(item['distances'])
+
     def add_location(self, x, y):
         self.last_location += 1
-        self.locations = {'id': self.last_location, 'area': 1}
-        self.field[y][x] = {'location_id': str(self.last_location), 'distance': 0}
+        self.locations[str(self.last_location)] = {'center': [x,y], 'area': 1}
+        self.field[x][y]['location_id'] =  str(self.last_location)
+        self.field[x][y]['distances'].append(0)
+        self._fill_area(self.locations[str(self.last_location)], self.last_location)
 
-    def _distance(self, point_1, point_2):
-        return (point_1[0] - point_2[0]) + (point1[1] - point_2[1])
-    def _calculate_area(self, location):
-        for y, row in enumerate(self.field):
-            for x, item in enumerate(row):
-                # TODO: Complete this
-                pass
+    def calculate_area(self):
+        for x, row in enumerate(self.field):
+            for y, item in enumerate(row):
+                if item['location_id'] != '.' and self.locations[item['location_id']]['area'] != -1:
+                    if x not in self.x_bounds and y not in self.y_bounds:
+                        self.locations[item['location_id']]['area'] += 1
+                    else:
+                        self.locations[item['location_id']]['area'] = -1
 
+    def calculate_max_dist_area(self, max_distance):
+        area = 0
+        self._calculate_distance_sum()
+        for x, row in enumerate(self.field):
+            for y, item in enumerate(row):
+                if item['total_distance'] < max_distance:
+                    area += 1
+        return area
+
+    def print_areas(self):
+        for id, item in self.locations.items():
+            print(f'Field: {id}, Area: {item["area"]}\n')
+
+    def max_area(self):
+        max_area = 0
+        for item in self.locations.values():
+            if item['area'] > max_area:
+                max_area = item['area']
+        return max_area
+
+    def _print_to_log(self):
+        with open('data/day6.log', 'w') as f:
+            f.write(str(self))
 
     def __str__(self):
         output = ''
         for row in self.field:
-            for item in row:
-                output += item['location_id']
-            output += '\n'
+            output += "\t".join([i["location_id"] for i in row]) + '\n'
         return output
 
 def get_data(filename):
@@ -38,14 +82,26 @@ def scale(data):
     x_min, y_min = (min([i[0] for i in data]), min([i[1] for i in data]))
     return [[i[0] - x_min, i[1] - y_min] for i in data]
 
-def part_one(filename):
+def day_6(filename, max_distance, pretty_print=False, print_to_log=False):
     data = get_data(filename)
     data = scale(data)
-    x_limit, y_limit = (max([i[0] for i in data]), max([i[1] for i in data]))
+    x_limit, y_limit = (max([i[1] for i in data]), max([i[0] for i in data]))
     field = Field(x_limit, y_limit)
     for item in data:
         field.add_location(item[0], item[1])
-    return field
+    field.calculate_area()
+    if pretty_print:
+        field.print_areas()
+        print(field)
+    if print_to_log:
+        with open('data/day6.log', 'w') as f:
+            f.write(str(field))
+    return (field.max_area(), field.calculate_max_dist_area(max_distance))
 
 if __name__ == '__main__':
-    print(part_one('day6-test.txt'))
+    # max_dist = 32
+    # part_one, part_two = day_6("day6-test.txt", max_dist)
+    max_dist = 10000
+    part_one, part_two = day_6("day6.txt", max_dist)
+    print(f'The max, non infinite, area is {part_one}')
+    print(f'The area of the region containing all locations with a sum distance less than {max_dist} is {part_two}')
